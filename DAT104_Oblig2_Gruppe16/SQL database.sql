2e50f6a5a60b84fa1d594e5725c0b9f020bd1941
h181185 <jacobaas@hotmail.com>
2017-09-28 15:00:46 +0200

    Laget SQL

---
+++ DAT104_Oblig2_Gruppe16/SQL database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE fest.bruker
+(
+  brukernavn character varying(50) NOT NULL,
+  passord character varying(50) NOT NULL,
+  telefonnummer character varying(8) NOT NULL,
+  primary key (brukernavn)
+)
--- DAT104_Oblig2_Gruppe16/src/no/hvl/dat104/modell/Administrator.java
+++ DAT104_Oblig2_Gruppe16/src/no/hvl/dat104/modell/Administrator.java
@@ -3,6 +3,7 @@ package no.hvl.dat104.modell;
 public class Administrator {
 
 	String navn;
-	
+
 	String undertøy;
+
 }
