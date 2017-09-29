ac08071404d504adb11125dd096e741e0aa07e38
h181185 <jacobaas@hotmail.com>
2017-09-29 11:43:34 +0200

    SQL tabell bruker ferdig

--- DAT104_Oblig2_Gruppe16/SQL/SQLdatabase.sql
+++ DAT104_Oblig2_Gruppe16/SQL/SQLdatabase.sql
@@ -1,9 +1,14 @@
-CREATE TABLE fest.bruker
-(
+CREATE SCHEMA IF NOT EXISTS fest;
+
+CREATE DOMAIN gender CHAR(1)
+    CHECK (value IN ( 'F' , 'M' ) );
+
+CREATE TABLE fest.bruker (
   brukernavn character varying(50) NOT NULL,
   etternavn character varying(50) NOT NULL,
-  kjonn character varying(50) NOT NULL,
+  kjonn fest.gender NOT NULL,
   telefonnummer character varying(8) NOT NULL,
   betalingsstatus BOOLEAN,
   primary key (telefonnummer)
-)
+);
+
