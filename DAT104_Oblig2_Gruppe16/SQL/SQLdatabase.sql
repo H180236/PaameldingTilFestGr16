7336278c62d74e0bceec3ad61a7faac582b21155
Øystein Knudsen <h180236@158.37.235.241>
2017-09-29 15:46:01 +0200

    Fikset database definisjon

--- DAT104_Oblig2_Gruppe16/SQL/SQLdatabase.sql
+++ DAT104_Oblig2_Gruppe16/SQL/SQLdatabase.sql
@@ -1,13 +1,15 @@
 CREATE SCHEMA IF NOT EXISTS fest;
 
-CREATE DOMAIN gender CHAR(1)
-    CHECK (value IN ( 'F' , 'M' ) );
 
-CREATE TABLE fest.bruker (
-  brukernavn character varying(50) NOT NULL,
-  etternavn character varying(50) NOT NULL,
+SET search_path TO fest;
+
+DROP TABLE IF EXISTS deltaker;
+
+CREATE TABLE fest.deltaker (
+  fornavn character varying(20) NOT NULL,
+  etternavn character varying(20) NOT NULL,
   kjonn fest.gender NOT NULL,
-  telefonnummer character varying(8) NOT NULL,
+  telefonnummer character(8) NOT NULL,
   betalingsstatus BOOLEAN,
   primary key (telefonnummer)
 );
---
+++ DAT104_Oblig2_Gruppe16/src/no/hvl/dat104/datatilgang/DeltakerEAO.java
@@ -0,0 +1,21 @@
+package no.hvl.dat104.datatilgang;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import no.hvl.dat104.modell.Deltaker;
+
+@Stateless
+public class DeltakerEAO {
+	
+	@PersistenceContext(name = "deltakerPersistenceUnit")
+	private EntityManager em;
+
+	private Deltaker finnDeltaker (String mobilnummer) {
+		return em.find(Deltaker.class,mobilnummer);
+	}
+	
+	
+
+}
