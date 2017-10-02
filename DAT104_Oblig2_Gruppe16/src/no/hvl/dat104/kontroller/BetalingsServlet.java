96d2fac9e88e39c3a85498f8b611a251b43b469e
karolineskylstad <karolineskylstad@158-37-231-81.hib.no>
2017-10-02 14:50:40 +0200

    servlets

---
+++ DAT104_Oblig2_Gruppe16/src/no/hvl/dat104/kontroller/BetalingsServlet.java
@@ -0,0 +1,66 @@
+
+package no.hvl.dat104.kontroller;
+
+import java.io.IOException;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import no.hvl.dat104.datatilgang.DeltakerEAO;
+import no.hvl.dat104.modell.Deltaker;
+
+/**
+ * Servlet implementation class BetalingsServlet
+ */
+@WebServlet("/BetalingsServlet")
+public class BetalingsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public BetalingsServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		
+		HttpSession sesjon = request.getSession();
+		if (sesjon.getAttribute("brukernavn").equals("91765536")) {
+			
+			request.getRequestDispatcher("/WEB-INF/betalingsoversikt.jsp").forward(request, response);
+		} else
+			request.getRequestDispatcher("LoginServlet").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@EJB
+	DeltakerEAO dEAO;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String telefonnummer = request.getParameter("telefonnummer");
+		dEAO.registrerBetaling(telefonnummer, true);
+		response.sendRedirect("BetalingsServlet");
+
+	}
+
+}
